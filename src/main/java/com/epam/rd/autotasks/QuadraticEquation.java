fa70ef3c78c785a5361547bcaeddf07724808a74
Ilya Mankouski <ilya.mankouski@gmail.com>
2022-01-11 13:53:35 +0100

    QuadraticEquation - Try1

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -11,8 +11,19 @@ public class QuadraticEquation {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
-
-
+        double D = Math.pow(b,2) - 4*a*c;
+        if (D>0){
+            double x1 = (-b + Math.sqrt(D))/(2*a);
+            double x2 = (-b - Math.sqrt(D))/(2*a);
+            System.out.print(x2 + " " + x1);
+        }
+        else if (D==0){
+            double x = (-b)/(2*a);
+            System.out.print(x);
+        }
+        else {
+            System.out.print("no roots");
+        }
 
     }
 
